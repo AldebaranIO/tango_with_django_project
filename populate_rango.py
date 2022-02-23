e2e7fd673defec80c1e2f8c4de979c7fc4345daa
Dongyao Wang <2612859w@student.gla.ac.uk>
2022-02-23 12:32:30 +0000

    chapter5 final version

--- populate_rango.py
+++ populate_rango.py
@@ -30,12 +30,12 @@ def populate():
         {'title':'Flask',
         'url':'http://flask.pocoo.org'} ]
 
-    cats = {'Python': {'pages': python_pages},
-            'Django': {'pages': django_pages},
-            'Other Frameworks': {'pages': other_pages} }
+    cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
+            'Django': {'pages': django_pages,'views': 64,'likes': 32 },
+            'Other Frameworks': {'pages': other_pages, 'views': 32 ,'likes': 16} }
 
     for cat, cat_data in cats.items():
-        c = add_cat(cat, cat_data['views'], cat_data['likes'])
+        c = add_cat(cat,cat_data['views'],cat_data['likes'])
         for p in cat_data['pages']:
             add_page(c, p['title'], p['url'])
 
